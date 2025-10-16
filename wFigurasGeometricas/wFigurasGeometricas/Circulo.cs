5e6f1a7bc9b3a4ac4eeb2e9519c82383436fa68d
JuanDnaranjo302 <saklaxy1111@gmail.com>
2025-10-15 22:06:27 -0500

    Clase Circulo

---
+++ wFigurasGeometricas/wFigurasGeometricas/Circulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wFigurasGeometricas
+{
+    public  class Circulo : Figura
+    {
+        public int Radio { get; set; }
+        public Circulo(Color color, Point posicion, int radio) : base(color, posicion)
+        {
+            Radio = radio;
+        }
+        public override void Dibujar(Graphics g)
+        {
+
+            SolidBrush blueBrush = new SolidBrush(Color);
+
+            g.FillEllipse(blueBrush, Posicion.X + Radio, Posicion.Y, Radio * 2, Radio * 2);
+
+        }
+
+    }
+}
