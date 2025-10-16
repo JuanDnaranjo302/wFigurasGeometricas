73d8d5072d12a94c67b3ad1d0b08c54afb4a52da
JuanDnaranjo302 <saklaxy1111@gmail.com>
2025-10-15 22:11:23 -0500

    Clase Linea

---
+++ wFigurasGeometricas/wFigurasGeometricas/Linea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wFigurasGeometricas
+{
+    public class Linea : Figura
+    {
+        public Point Fin { get; set; }
+        public Linea(Color color, Point posicion, Point fin) : base(color, posicion)
+        {
+            Fin = fin;
+        }
+        public override void Dibujar(Graphics g)
+        {
+            Pen blackpen = new Pen(Color, 3);
+            g.DrawLine(blackpen, Posicion, Fin);
+        }
+    }
+}
