df785df09b4b258c17537bb7daf875a753181463
JuanDnaranjo302 <saklaxy1111@gmail.com>
2025-10-15 22:00:07 -0500

    Clase Figura

---
+++ wFigurasGeometricas/wFigurasGeometricas/Figura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace wFigurasGeometricas
+{
+    public abstract class Figura
+    {
+        public Color Color { get; set; }
+        public Point Posicion { get; set; }
+
+        public Figura(Color color, Point posicion)
+        {
+            Color = color;
+            Posicion = posicion;
+        }
+        public abstract void Dibujar(Graphics g);
+    }
+}
